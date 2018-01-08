fbe74a15c23b6d00658a7fc22587a7536e35c741
alganbr <algan_br@hotmail.com>
2018-01-08 13:43:56 -0800

    Integrated attributes validation for blogs and projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,8 +3,9 @@ class Project < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
 
-  after_initialize :set_defaults
+  validates_presence_of :title, :body, :main_image, :thumb_image
 
+  after_initialize :set_defaults
   def set_defaults
     self.main_image ||= PlaceHolder.image_generator(height: '600', width: '400')
     self.thumb_image ||= PlaceHolder.image_generator(height: '350', width: '200')
