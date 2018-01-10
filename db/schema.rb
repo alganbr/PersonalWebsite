ce5e0f045cbd21d6b7a354fbb46d1ada2f204939
alganbr <algan_br@hotmail.com>
2018-01-10 14:32:32 -0800

    Integrated grid system to project

--- db/schema.rb
+++ db/schema.rb
@@ -25,16 +25,6 @@ ActiveRecord::Schema.define(version: 20180108211229) do
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
-  create_table "portfolios", force: :cascade do |t|
-    t.string "title"
-    t.string "subtitle"
-    t.text "body"
-    t.text "main_image"
-    t.text "thumb_image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.string "subtitle"
@@ -60,7 +50,6 @@ ActiveRecord::Schema.define(version: 20180108211229) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin", default: false
     t.string "roles"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
