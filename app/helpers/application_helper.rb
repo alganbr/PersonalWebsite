9d0ce5d1866331fc3af1d34d6d9b45291e8afbb3
alganbr <algan_br@hotmail.com>
2018-01-11 13:34:11 -0800

    Integrated dynamic navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,43 @@ module ApplicationHelper
   def copyright_generator
     "@2018 | <strong>Algan Rustinya</strong> All rights reserved".html_safe
   end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_me_path,
+        title: 'About Me'
+      },
+      {
+        url: projects_path,
+        title: 'Projects'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
 end
